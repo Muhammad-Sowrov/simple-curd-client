b04d6363bc3228c400e091a681f9a571a9a38cec
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-10-15 13:50:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  return (
+    <>
+      
+      <h1>CRUD MANAGEMENT</h1>
+      
+    </>
+  )
+}
+
+export default App
